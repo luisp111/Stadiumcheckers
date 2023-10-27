489a39f428e0eeb118696dbbd08dac531e3fc698
Luispr11 <luisperezruiz971@gmail.com>
2023-10-26 18:19:30 -0700

    buttons

--- app/src/main/java/edu/up/cs301/stadiumcheckers/SCMainActivity.java
+++ app/src/main/java/edu/up/cs301/stadiumcheckers/SCMainActivity.java
@@ -1,4 +1,18 @@
 package edu.up.cs301.stadiumcheckers;
 
-public class SCMainActivity {
+import edu.up.cs301.game.GameFramework.GameMainActivity;
+import edu.up.cs301.game.GameFramework.LocalGame;
+import edu.up.cs301.game.GameFramework.gameConfiguration.GameConfig;
+import edu.up.cs301.game.GameFramework.infoMessage.GameState;
+
+public class SCMainActivity extends GameMainActivity {
+    @Override
+    public GameConfig createDefaultConfig() {
+        return null;
+    }
+
+    @Override
+    public LocalGame createLocalGame(GameState gameState) {
+        return null;
+    }
 }
--- app/src/main/java/edu/up/cs301/stadiumcheckers/players/SCHumanPlayer.java
+++ app/src/main/java/edu/up/cs301/stadiumcheckers/players/SCHumanPlayer.java
@@ -1,4 +1,39 @@
 package edu.up.cs301.stadiumcheckers.players;
 
-public class SCHumanPlayer {
+import android.view.View;
+import android.widget.Button;
+import edu.up.cs301.game.GameFramework.GameMainActivity;
+import edu.up.cs301.game.GameFramework.infoMessage.GameInfo;
+import edu.up.cs301.game.GameFramework.players.GameHumanPlayer;
+import edu.up.cs301.game.R;
+
+public class SCHumanPlayer extends GameHumanPlayer implements View.OnClickListener{
+    /**
+     * constructor
+     *
+     * @param name the name of the player
+     */
+    public SCHumanPlayer(String name) {
+        super(name);
+    }
+
+    @Override
+    public View getTopView() {
+        return myActivity.findViewById((R.id.human_player_view);
+    }
+
+    @Override
+    public void receiveInfo(GameInfo info) {
+
+    }
+
+    @Override
+    public void setAsGui(GameMainActivity activity) {
+            myActivity = activity;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
 }
